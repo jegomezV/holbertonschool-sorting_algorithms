6d73b40062b7410880e073f0161f521f2c848d03
jegomezV <jegomezvalencia@gmail.com>
2023-05-16 12:23:32 -0500

    Bubble

--- print_array.c
+++ print_array.c
@@ -1,26 +1,5 @@
-#include "sort.h"
-
-/**
- * print_list - Prints a list of integers
- *
- * @list: The list to be printed
- */
-void print_list(const listint_t *list)
-{
-    int i;
-
-    i = 0;
-    while (list)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", list->n);
-        ++i;
-        list = list->next;
-    }
-    printf("\n");
-}
-
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * print_array - Prints an array of integers
--- sort.h
+++ sort.h
@@ -22,6 +22,7 @@ typedef struct listint_s
 /*                 PROTOTYPES                 */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
 
 
 #endif
